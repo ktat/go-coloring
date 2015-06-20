c83b8124d5ecaf21bf0c0e1bf72c3895a576ad8f
ktat <ktat.is@gmail.com>
2015-06-20 22:21:49 +0900

    separater pager

--- coloring.go
+++ coloring.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/fuzzy/gocolor"
-	"github.com/ktat/go-coloring/coloring"
+	"github.com/ktat/go-pager"
 	. "github.com/mattn/go-getopt"
 	"io/ioutil"
 	"log"
