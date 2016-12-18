6b2d9dae1b939fe81be4411ab0425e552f4f114f
ktat <ktat.is@gmail.com>
2016-12-18 21:48:07 +0900

    add reset before line break and add color after line break

--- coloring/color.go
+++ coloring/color.go
@@ -2,10 +2,29 @@ package coloring
 
 import (
 	"fmt"
+	"regexp"
+)
+
+const (
+	BLACK   = "\033[30m"
+	RED     = "\033[31m"
+	GREEN   = "\033[32m"
+	YELLOW  = "\033[33m"
+	BLUE    = "\033[34m"
+	MAGENTA = "\033[35m"
+	CYAN    = "\033[36m"
+	WHITE   = "\033[37m"
+	BOLD    = "\033[1m"
+	RESET   = "\033[0m"
 )
 
 type String struct {
 	Str string
+}
+
+func (s String) resetWithLineBreak(color string) string {
+	var r = regexp.MustCompile("(?s)([\r\n]+)")
+	return r.ReplaceAllString(s.Str, RESET+"$1"+color)
 }
 
 func (s String) String() string {
@@ -13,37 +32,37 @@ func (s String) String() string {
 }
 
 func (s String) Black() string {
-	return fmt.Sprintf("\033[30m%s\033[0m", s)
+	return fmt.Sprintf(BLACK+"%s"+RESET, s.resetWithLineBreak(BLACK))
 }
 
 func (s String) Red() string {
-	return fmt.Sprintf("\033[31m%s\033[0m", s)
+	return fmt.Sprintf(RED+"%s"+RESET, s.resetWithLineBreak(RED))
 }
 
 func (s String) Green() string {
-	return fmt.Sprintf("\033[32m%s\033[0m", s)
+	return fmt.Sprintf(GREEN+"%s"+RESET, s.resetWithLineBreak(GREEN))
 }
 
 func (s String) Yellow() string {
-	return fmt.Sprintf("\033[33m%s\033[0m", s)
+	return fmt.Sprintf(YELLOW+"%s"+RESET, s.resetWithLineBreak(YELLOW))
 }
 
 func (s String) Blue() string {
-	return fmt.Sprintf("\033[34m%s\033[0m", s)
+	return fmt.Sprintf(BLUE+"%s"+RESET, s.resetWithLineBreak(BLUE))
 }
 
 func (s String) Magenta() string {
-	return fmt.Sprintf("\033[35m%s\033[0m", s)
+	return fmt.Sprintf(MAGENTA+"%s"+RESET, s.resetWithLineBreak(MAGENTA))
 }
 
 func (s String) Cyan() string {
-	return fmt.Sprintf("\033[36m%s\033[0m", s)
+	return fmt.Sprintf(CYAN+"%s"+RESET, s.resetWithLineBreak(CYAN))
 }
 
 func (s String) White() string {
-	return fmt.Sprintf("\033[37m%s\033[0m", s)
+	return fmt.Sprintf(WHITE+"%s"+RESET, s.resetWithLineBreak(WHITE))
 }
 
 func (s String) Bold() string {
-	return fmt.Sprintf("\033[1m%s\033[0m", s)
+	return fmt.Sprintf(BOLD+"%s"+RESET, s.resetWithLineBreak(BOLD))
 }
